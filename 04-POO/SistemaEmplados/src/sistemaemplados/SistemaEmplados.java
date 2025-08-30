28fba3a2a5384c69ea9ebb28ac57fa7695818c7b
salocin-95 <renicolas7@gmail>
2025-08-30 18:37:21 -0300

    Add folders

---
+++ 04-POO/SistemaEmplados/src/sistemaemplados/SistemaEmplados.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemaemplados;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class SistemaEmplados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
