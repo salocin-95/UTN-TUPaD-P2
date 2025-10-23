913aa9fccd530c794547bac4aaf56cd0722aadb4
salocin-95 <renicolas7@gmail>
2025-10-22 21:13:36 -0300

    Añade la soluciones al prtimer ejercicio

---
+++ 07-Herencia-y-Polimorfismo/Vehiculo/src/vehiculo/Vehiculo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vehiculo;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Vehiculo {
+    protected String marca;
+    protected String modelo;
+
+    public Vehiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
+    public void mostrarInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+    }
+}
